d2e29ddef7b8254b025d0e63c8c0b01fdc361ae1
Matej Dedina <gameplay.nano@gmail.com>
2025-11-18 21:00:10 +0100

    obfuscated code

--- include/cerpec.h
+++ include/cerpec.h
@@ -14,8 +14,8 @@ typedef void * (*copy_fn)    (void * destination, const void * source);
 typedef size_t (*hash_fn)    (const void * element);
 typedef int    (*compare_fn) (const void * a, const void * b);
 typedef bool   (*filter_fn)  (const void * element, void * arguments);
-typedef bool   (*handle_fn) (void * element, void * arguments);
-typedef void   (*process_fn)  (void * array, const size_t lenght, void * arguments);
-typedef void   (*operate_fn)  (void * a, void * b, const void * result);
+typedef bool   (*handle_fn)  (void * element, void * arguments);
+typedef void   (*process_fn) (void * array, const size_t lenght, void * arguments);
+typedef void   (*operate_fn) (void * a, void * b, const void * result);
 
 #endif // CERPEC_H
